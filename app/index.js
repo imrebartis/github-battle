097bb10bbfdcad4b7e8f480aca4d0534d51f52f0
imrebartis <imbartis@gmail.com>
2017-07-27 08:07:18 +0300

    using map part 1

--- app/index.js
+++ app/index.js
@@ -7,50 +7,19 @@ require('./index.css');
 // UI (see component definition, almost always needed)
 
 //component definition
-
-
-class Avatar extends React.Component {
+class Users extends React.Component {
   render() {
     return (
-      <img src={this.props.img} />
-    )
-  }
-}
-
-class Label extends React.Component {
-  render() {
-    return (
-      <h1>Name: {this.props.name} </h1>
-    )
-  }
-}
-
-class ScreenName extends React.Component {
-  render() {
-    return (
-      <h3>Username: {this.props.username} </h3>
-    )
-  }
-}
-
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <Avatar img={this.props.user.img}/>
-        <Label name={this.props.user.name}/>
-        <ScreenName username={this.props.user.username}/>
-      </div>
+      <ul>
+        {this.props.list.map(function(user){
+            return <li> {user} </li>;
+          })}
+      </ul>
     )
   }
 }
 
 ReactDOM.render(
-  <Badge user={{
-    name: 'Cat Stevens',
-    img: 'http://kaboompics.com/cache/4/a/e/9/b/4ae9b5eb5c90b004225674f6aa75fe531458fe8c.jpeg?version=v17',
-    username: 'cat'
-  }} />,
+  <Users list={['Tyler', 'Mikenzi', 'Ryan', 'Michael']} />,
   document.getElementById('app')
 );
-
