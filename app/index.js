f1320c89e1a9d6c5d8d01d9489c302bd0c831e6e
imrebartis <imbartis@gmail.com>
2017-07-27 14:20:01 +0300

    map&filter exercise done

--- app/index.js
+++ app/index.js
@@ -9,17 +9,48 @@ require('./index.css');
 //component definition
 class Users extends React.Component {
   render() {
+
+     var friends = this.props.list.filter(function (user) {
+              return user.friend === true;
+            });
+
+    var nonFriends = this.props.list.filter(function (user) {
+              return user.friend !== true;
+            });
+
     return (
-      <ul>
-        {this.props.list.map(function(user){
-            return <li> {user} </li>;
-          })}
-      </ul>
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {/*Create an <li> for every name in the list array who is also your friend*/
+            friends.map(function (user){
+              // key is added so that each item could have a unique identifier
+              return <li key={user.name}>{user.name}</li>
+            })}
+        </ul>
+        
+        <hr />
+        
+        <h1> Non Friends </h1>
+        <ul>
+          {/*Create an <li> for every name in the list array who is NOT your friend*/
+               nonFriends.map(function (user){
+              return <li key={user.name}>{user.name}</li>
+            })}
+        </ul>        
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Users list={['Tyler', 'Mikenzi', 'Ryan', 'Michael']} />,
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]} 
+  />,
   document.getElementById('app')
 );
